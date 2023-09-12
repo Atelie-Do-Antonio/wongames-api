8ebd245f8eb2e12e5f1d12fedd50d4be5d5dfade
Antonio Silva <antoniobsilvaprof@hotmail.com>
2023-09-12 09:54:39 -0300

    feat: start a custom public route

--- src/api/game/controllers/game.ts
+++ src/api/game/controllers/game.ts
@@ -2,6 +2,15 @@
  * game controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::game.game');
+export default factories.createCoreController(
+  "api::game.game",
+  ({ strapi }) => ({
+    async populate(ctx) {
+      console.log("Rodando no servidor")
+      // const data = await strapi.services.game.populate();
+      ctx.send("Finalizado no client");
+    },
+  })
+);
