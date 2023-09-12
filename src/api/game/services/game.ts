f4c1d1ecd08375467fdd385c96872f22f1b90290
Antonio Silva <antoniobsilvaprof@hotmail.com>
2023-09-12 15:56:33 -0300

    feat: import cover and gallery

--- src/api/game/services/game.ts
+++ src/api/game/services/game.ts
@@ -97,6 +97,31 @@ async function createManyToManyData(products) {
   ]);
 }
 
+async function setImage({ image, game, field = "cover" }) {
+  const { data } = await axios.get(image, { responseType: "arraybuffer" });
+  const buffer = Buffer.from(data, "base64");
+
+  const FormData = require("form-data");
+
+  const formData: any = new FormData();
+
+  formData.append("refId", game.id);
+  formData.append("ref", `${gameService}`);
+  formData.append("field", field);
+  formData.append("files", buffer, { filename: `${game.slug}.jpg` });
+
+  console.info(`Uploading ${field} image: ${game.slug}.jpg`);
+
+  await axios({
+    method: "POST",
+    url: `http://localhost:1337/api/upload/`,
+    data: formData,
+    headers: {
+      "Content-Type": `multipart/form-data; boundary=${formData._boundary}`,
+    },
+  });
+}
+
 async function createGames(products) {
   await Promise.all(
     products.map(async (product) => {
@@ -134,6 +159,20 @@ async function createGames(products) {
           },
         });
 
+        await setImage({ image: product.coverHorizontal, game });
+        await Promise.all(
+          product.screenshots.slice(0, 5).map((url) =>
+            setImage({
+              image: `${url.replace(
+                "{formatter}",
+                "product_card_v2_mobile_slider_639"
+              )}`,
+              game,
+              field: "gallery",
+            })
+          )
+        );
+
         return game;
       }
     })
@@ -148,9 +187,9 @@ export default factories.createCoreService(gameService, () => ({
       data: { products },
     } = await axios.get(gogApiUrl);
 
-    await createManyToManyData([products[1], products[3]]);
+    await createManyToManyData([products[0], products[2]]);
 
-    await createGames([products[1], products[3]]);
+    await createGames([products[0], products[2]]);
 
     // console.log(await getGameInfo(products[2].slug));
   },
