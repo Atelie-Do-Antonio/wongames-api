9f0418979415c5931d5c721750aafe781196ecb7
Antonio Silva <antoniobsilvaprof@hotmail.com>
2023-09-12 16:37:35 -0300

    feat: customazing api call with query params

--- src/api/game/controllers/game.ts
+++ src/api/game/controllers/game.ts
@@ -8,12 +8,15 @@ export default factories.createCoreController(
   "api::game.game",
   ({ strapi }) => ({
     async populate(ctx) {
-      console.log("Starting to populate...")
+      const options = {
+        limit: 48,
+        order: "desc:trending",
+        ...ctx.query,
+      };
 
-      await strapi.service("api::game.game").populate(ctx.query);
+      await strapi.service("api::game.game").populate(options);
 
-      // const data = await strapi.services.game.populate();
-      ctx.send("Finished populating!");
+      ctx.send("Finished populating games!");
     },
   })
 );
